941293c8befce5233c7a3a5e460ee72d2c4beb75
Fitsum Seyoum <fitsum.s@yahoo.com>
2019-05-04 21:37:15 -0400

    commited my second

--- src/com/fitsum/Main.java
+++ src/com/fitsum/Main.java
@@ -3,6 +3,19 @@ package com.fitsum;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //
+        int myInt = 5;
+        float myFloat = 5;
+        double myDobule = 5;
+
+        System.out.println("Int value" + (myInt)/3);
+        System.out.println("Float goes to 7 digits after decimal that is single precision");
+        System.out.println("Float has single prcesion" + (myFloat)/3);
+        System.out.println("Dobule goes to 16 digits after decimal it is called double precision");
+        System.out.println("Double has double precision = " + (myDobule)/3);
+
+
+
     }
 }
