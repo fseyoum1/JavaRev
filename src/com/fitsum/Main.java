5f82d74e7b28d2cad9d87c33f94802ee3ee67f7e
Fitsum Seyoum <fitsum.s@yahoo.com>
2019-05-04 22:38:31 -0400

    added some

--- src/com/fitsum/Main.java
+++ src/com/fitsum/Main.java
@@ -14,7 +14,20 @@ public class Main {
         System.out.println("Float has single prcesion" + (myFloat)/3);
         System.out.println("Dobule goes to 16 digits after decimal it is called double precision");
         System.out.println("Double has double precision = " + (myDobule)/3);
-
+        /*
+        if we do not specify using f the default is double
+        if we do not specify whole numbers the default is int
+        see the below example for type cast error that can happend because of this
+        * */
+        // byte has a width of 8 -> -128 to 127
+        // assigning literal in java is fine here we are assigning a literal which is a constant number
+        // so java will not consider it as integer instead it will consider it as byte"since it is literal"
+        // not the case if it was an expression
+        byte myByte = 127;
+        // passing the variable name and dividing it is an error this is because java consider
+        // this as an expression not literal, so when java computes the value it defaults it to an integer
+        // that is why it is not working the way to go around is to type cast it to byte
+        myByte = (byte) ((myByte)/3); 
 
 
     }
